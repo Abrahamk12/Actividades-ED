97c1efd39431936e12778419ed3d1d6126fa732f
Uriel Abraham <abraham2160@hotmail.com>
2025-02-14 08:05:24 -0700

    Recursividad

--- ActividadesED/src/main/java/com/mycompany/actividadesed/ActividadesED.java
+++ ActividadesED/src/main/java/com/mycompany/actividadesed/ActividadesED.java
@@ -8,12 +8,20 @@ public class ActividadesED {
         Operaciones op = new Operaciones();
         op.setDimension(2);
         op.setDatosArreglo(5, 1);
-        */
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un numero");
         int numero = sc.nextInt();
         System.out.println("Ingrese un texto");
         String t = sc.nextLine();
         System.out.println(numero + "\nTexto: " + t);
+        */
+        imprimirNumeros(0, 15);
+    }
+    
+    public static void imprimirNumeros(int inicio, int fin){
+        if(inicio <= fin){
+            System.out.println(inicio);
+            imprimirNumeros(inicio + 1, fin);
+        }
     }
 }
